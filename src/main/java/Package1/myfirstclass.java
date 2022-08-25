8b7b7c52f5b8ed147aae59f5c3bcd9df837ec924
unknown <rohit09004@gmail.com>
2022-08-25 23:22:55 +0530

    updates stag changes

--- src/main/java/Package1/myfirstclass.java
+++ src/main/java/Package1/myfirstclass.java
@@ -5,5 +5,6 @@ public class myfirstclass {
 	public static void main(String[] args) {
 		System.out.println("Its Flipkart Application");
 		System.out.println("Its Flipkart Application second time");
+		
 	}	
 	}
